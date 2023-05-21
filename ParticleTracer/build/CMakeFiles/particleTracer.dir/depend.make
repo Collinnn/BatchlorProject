50b3ee3080d5b08901525f4069e3dc8623e87128
Collinnn <51782585+Collinnn@users.noreply.github.com>
2023-05-21 19:29:38 +0200

    Working example without cmake

--- ParticleTracer/build/CMakeFiles/particleTracer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for particleTracer.
-# This may be replaced when dependencies are built.
--- ParticleTracer/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Tobias Collin/Documents/GitHub/BatchlorProject/ParticleTracer
-# Build directory: C:/Users/Tobias Collin/Documents/GitHub/BatchlorProject/ParticleTracer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
